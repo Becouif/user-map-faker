8717cf78db21f408c5c74c89ae488806a5e5e140
becouif <lovguya@gmail.com>
2022-07-24 00:46:23 +0300

    updated

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,6 @@
-console.log('Hello world');
-export {};
+import { User } from './components/User';
+import { Company } from './components/Company';
+const user = new User();
+const company = new Company();
+console.log(company);
+console.log(user);
